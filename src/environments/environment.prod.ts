cf2c21f73ee2bff075a1d0190112cf579073cb54
Henry Overholt <henrysoverholt@gmail.com>
2020-08-04 12:25:55 -0400

    changed environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ import { openWeather } from './openWeather.environment';
 export const environment = {
   production: true,
   openWeather: {
-    apiKey: openWeather.openWeather.apiKey,
+    apiKey: 'undefined',
   },
 };
